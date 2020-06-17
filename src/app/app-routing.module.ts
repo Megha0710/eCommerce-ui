19de6c97e24cddcce1dca755b234dc20f19560c0
meghamalhotra-saggezza <megha.malhotra@saggezza.com>
2020-06-17 15:07:18 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { ShopComponent } from './shop/shop.component';
+import { ProductsResolverService } from './products/product-resolver.service';
+import { CartComponent } from './cart/cart.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:ShopComponent , resolve: [ProductsResolverService]},
+  {path:'shop',component:ShopComponent, resolve: [ProductsResolverService] },
+  {path:'cart',component:CartComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductService } from './products/product.service';
+import { ShopComponent } from './shop/shop.component';
+import { CartComponent } from './cart/cart.component';
+import { CartItemComponent } from './cart/cart-item/cart-item.component';
+import { DropdownDirective } from './shared/dropdown.directive';
+import { FooterComponent } from './footer/footer.component';
+import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinner.component';
+import { CartService } from './cart/cart.service';
+import { QuantityControlComponent } from './quantity-control/quantity-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ProductsComponent,
+    ShopComponent,
+    CartComponent,
+    CartItemComponent,
+    DropdownDirective,
+    FooterComponent,
+    LoadingSpinnerComponent,
+    QuantityControlComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [ProductService,CartService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
